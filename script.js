910e2f98de30ae85d631cbfaf1bc8bdc19d4e347
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-06-30 09:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let arr = [1, 2, 3, 4]
+let filtererArr  = new Promise((resolve,reject)=>{
+	let finalArr = []
+	setTimeout(()=>{
+	 arr.filter((item)=>{
+			if(item%2 == 0){
+				finalArr.push(item)
+			}
+		})
+		resolve(finalArr);
+		
+	},3000)
+})
+
+filtererArr.then((value)=>{
+	setTimeout(()=>{
+		document.getElementById("output").innerText = value.join(", ")
+	},1000)
+	
+	let MultiArr = value.map((ele)=>ele*2)
+	return MultiArr;
+}).then((res)=>{
+	setTimeout(()=>{
+		document.getElementById("output").innerText = res.join(", ")
+	},2000)
+})
